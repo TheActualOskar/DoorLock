9489a887f8df7823d973e90b03170770168132de
Joachim Baumann <jobau19@student.sdu.dk>
2024-05-06 22:40:14 +0200

    added option to publish messages to MQTT, needs coordination with smart door lock.

--- doorlock-backend/src/main/java/dk/sdu/FreeKeyLocks/doorlock/Controllers/MqttController.java
+++ doorlock-backend/src/main/java/dk/sdu/FreeKeyLocks/doorlock/Controllers/MqttController.java
@@ -2,6 +2,7 @@ package dk.sdu.FreeKeyLocks.doorlock.Controllers;
 
 import com.github.tocrhz.mqtt.annotation.MqttSubscribe;
 import com.github.tocrhz.mqtt.annotation.Payload;
+import com.github.tocrhz.mqtt.publisher.MqttPublisher;
 import dk.sdu.FreeKeyLocks.doorlock.Model.DoorLock;
 import dk.sdu.FreeKeyLocks.doorlock.Model.HeartBeat;
 import dk.sdu.FreeKeyLocks.doorlock.Model.LogEntry;
@@ -11,9 +12,12 @@ import dk.sdu.FreeKeyLocks.doorlock.Repository.LogEntryRepository;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
-
+@CrossOrigin("*")
+@RequestMapping("/test")
+@RestController
 @Component
 public class MqttController {
     @Autowired
@@ -23,11 +27,25 @@ public class MqttController {
     @Autowired
     LogEntryRepository logEntryRepository;
 
+    private static MqttPublisher publisher;
 
-    @MqttSubscribe("sensor/unlock/1")
+    public MqttController(MqttPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+
+    //TODO add unlock logic
+    @GetMapping("/{id}")
+    public static void unlockDoorLock(@PathVariable("id") int id) {
+
+        publisher.send("sensor/unlockdoor/" + id,"Hello",2);
+    }
+
+
+    //TODO is this usefull?
+    @MqttSubscribe("sensor/unlock/+")
     public void mqttrecieve(String topic, MqttMessage message, @Payload String payload) {
         System.out.println("message from the gods: " + payload);
-
     }
 
     @MqttSubscribe("sensor/log/+")
