634fb586b8b34f1e8383a5a571dbc10b1e471af2
Joachim Baumann <jobau19@student.sdu.dk>
2024-05-06 15:30:26 +0200

    added MQTT support for subscribing and saving Heatbeats and logs

---
+++ doorlock-backend/src/main/java/dk/sdu/FreeKeyLocks/doorlock/Controllers/MqttController.java
@@ -0,0 +1,67 @@
+package dk.sdu.FreeKeyLocks.doorlock.Controllers;
+
+import com.github.tocrhz.mqtt.annotation.MqttSubscribe;
+import com.github.tocrhz.mqtt.annotation.Payload;
+import dk.sdu.FreeKeyLocks.doorlock.Model.DoorLock;
+import dk.sdu.FreeKeyLocks.doorlock.Model.HeartBeat;
+import dk.sdu.FreeKeyLocks.doorlock.Model.LogEntry;
+import dk.sdu.FreeKeyLocks.doorlock.Repository.DoorLockRepository;
+import dk.sdu.FreeKeyLocks.doorlock.Repository.HeatBeatRepository;
+import dk.sdu.FreeKeyLocks.doorlock.Repository.LogEntryRepository;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+
+@Component
+public class MqttController {
+    @Autowired
+    DoorLockRepository doorLockRepository;
+    @Autowired
+    HeatBeatRepository heatBeatRepository;
+    @Autowired
+    LogEntryRepository logEntryRepository;
+
+
+    @MqttSubscribe("sensor/unlock/1")
+    public void mqttrecieve(String topic, MqttMessage message, @Payload String payload) {
+        System.out.println("message from the gods: " + payload);
+
+    }
+
+    @MqttSubscribe("sensor/log/+")
+    public void mqttrecieveLog(String topic, MqttMessage message, @Payload String payload) {
+        LogEntry log = new LogEntry();
+
+        String[] temp_id = topic.split("/");
+        int id = Integer.parseInt(temp_id[2]);
+        DoorLock doorLock = doorLockRepository.getReferenceById(id);
+
+
+        log.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        log.setDoorLock(doorLock);
+        log.setMessage(message.toString());
+
+        logEntryRepository.save(log);
+
+    }
+
+    @MqttSubscribe("sensor/heartbeat/+")
+    public void mqttrecieveheartbeat(String topic, MqttMessage message, @Payload String payload) {
+        HeartBeat heartBeat = new HeartBeat();
+
+        String[] temp_id = topic.split("/");
+        int id = Integer.parseInt(temp_id[2]);
+        DoorLock doorLock = doorLockRepository.getReferenceById(id);
+
+
+        heartBeat.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        heartBeat.setDoorLock(doorLock);
+        heartBeat.setMessage(message.toString());
+
+        heatBeatRepository.save(heartBeat);
+
+
+    }
+}
